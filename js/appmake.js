58c9cd78238fcff6c067df36a2f0419800b84c2f
17hagiwara <17hagiwara@gmail.com>
2023-08-18 12:42:14 +0900

    task add

---
+++ js/appmake.js
@@ -0,0 +1,25 @@
+
+// window.onload = function () {
+//     const memoText = localStorage.getItem('memo');
+//     if (memoText) {
+//         document.getElementById('memo-text').value = memoText;
+//     }
+// };
+
+// // メモを保存する
+// document.getElementById('save-button').addEventListener('click', function () {
+//     const memoText = document.getElementById('memo-text').value;
+//     localStorage.setItem('memo', memoText);
+//     alert('メモが保存されました！');
+// });
+
+
+
+const value = 1;
+if (value) {
+    console.log(true);
+} else {
+    console.log(false);
+}
+
+
